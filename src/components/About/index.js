50f6cdd12125601db74e07baaa5b09b10774d8cd
flizyx <nicolizarazo19@gmail.com>
2022-07-23 10:28:03 -0500

    added about 2

---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = (props) => {
+    return (
+        <>
+            <p>Hello world</p>
+        </>
+    )
+}
+
+export default About
---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import About from '../About';
+
+const Header = (props) => {
+    return (
+        <>
+        {props.child}
+        </>
+        // <div>
+        //     <li>
+        //         <p>
+        //             Test
+        //         {/* <div>Imagine a {props.color} datepicker here.</div>; */}
+        //                {/* <About color="red" />; */}
+        //         </p>
+        //     </li>
+        // </div>
+    )
+}
+
+export default Header
